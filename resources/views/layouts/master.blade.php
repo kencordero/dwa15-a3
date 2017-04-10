8e7d684102d96857b61720398227946d63c073ab
Kenneth Cordero <kencordero@gmail.com>
2017-04-10 00:40:59 -0500

    Update favicon

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,9 +9,8 @@
     </title>
 
     <!-- image source: http://paulferrett.com/fontawesome-favicon/ -->
-    <link rel="shortcut icon" type="image/x-icon" href="/images/favicon-shopping-cart.ico">
-
-
+    <link rel="shortcut icon" type="image/x-icon" href="/images/favicon-credit-card.ico">
+    
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.7/united/bootstrap.min.css">
     <link rel="stylesheet" href="/styles/global.css">
 </head>
