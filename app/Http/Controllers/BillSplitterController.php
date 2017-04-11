c6aa7b10baa236b786c92e90a5f191d390df6695
Kenneth Cordero <kencordero@gmail.com>
2017-04-10 20:01:38 -0500

    calculate quotient

--- app/Http/Controllers/BillSplitterController.php
+++ app/Http/Controllers/BillSplitterController.php
@@ -10,7 +10,22 @@ class BillSplitterController extends Controller
         GET
         /
     */
-    public function form() {
-        return view('billsplit.form');
+    public function form(Request $request) {
+        $quotient = null;
+        if ($_GET) {
+            $total = $request->input("total", null);
+            $splits = $request->input("splits", null);
+            $tip = $request->input("tip", null);
+
+            if ($splits != 0)
+                $quotient = $total / $splits;
+            if ($total && $splits && $tip) {
+
+            }
+        }
+        return view('billsplit.form')->with([
+            "request" => $request,
+            "quotient" => $quotient
+        ]);
     }
 }
--- resources/views/billsplit/form.blade.php
+++ resources/views/billsplit/form.blade.php
@@ -8,22 +8,28 @@
     <h1 class="text-center">Bill Splitter</h1>
     <form method="get" action="/">
         <label for="total">Bill total:</label>
-        <input type="text" name="total" id="total"></br>
+        <input type="text" name="total" id="total" value="{{ $request->input("total") }}" required></br>
 
         <label for="splits">Splits: </label>
-        <input type="text" name="splits" id="splits"></br>
+        <input type="text" name="splits" id="splits" value="{{ $request->input("splits") }}" required></br>
 
         <label for="tip">Tip:</label>
         <select name="tip" id="tip" required>
             <option value="">Choose</option>
-            <option value="10">10%</option>
-            <option value="15">15%</option>
-            <option value="20">20%</option>
+            @for ($i = 0;  $i < 101; $i += 5)
+                <option value="{{ $i }}" @if ($request->input("tip") == $i){{ 'selected' }} @endif>{{ $i }}%</option>
+            @endfor
         </select></br>
 
         <label for="round">Round up?</label>
         <input type="checkbox" id="round" name="round"></br>
 
         <input type="submit" value="Calculate">
+        @if ($quotient)
+            <div class="alert alert-info">
+                <h1>Result</h1>
+
+            </div>
+        @endif
     </form>
 @endsection
